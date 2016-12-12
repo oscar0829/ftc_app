56561acc627c9c60b1a742ea8e1cef3d7ee9806c
oscar0829 <oscar.garcia0829@gmail.com>
2016-12-12 17:26:52 -0600

    CannonBot
    Recieves and launches the balls.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CannonBot.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by oxg0829 on 12/5/2016.
+ */
+
+public class CannonBot extends OpMode {
+    JoyStick js = new JoyStick();
+    TankDrive robot = new TankDrive();
+    DcMotor sucker;
+    DcMotor leftTrigger;
+
+    @Override
+    public void loop() {
+        double leftY = gamepad1.left_trigger;
+    }
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap, "leftMotor", "rightMotor");
+        js.init(gamepad1);
+        boolean suckerPower = gamepad1.dpad_up;
+
+        if (suckerPower == true) {
+            sucker.setPower(1);
+        } else {
+            sucker.setPower(0);
+        }
+
+        if (leftTrigger < 0) {
+            leftTrigger.setPower(-1);
+        }
+        else {
+            leftTrigger.setPower(0);
+        }
+    }
